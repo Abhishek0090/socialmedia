373289ce40ab27213e052be4d5e086c985b91125
Abhishek0090 <palabhishek411@gmail.com>
2022-11-12 17:52:49 +0530

    changes little bit

---
+++ assets/php/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+    //function for showing pages
+    
+    function showPage($page,$data=""){ 
+    include("assets/pages/$page.php");
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+
+    // include('assets/pages/header.php');
+    // include('assets/pages/signup.php');
+    // include('assets/pages/footer.php');
+
+    require_once 'assets/php/functions.php';
+
+
+
+    if(isset($_GET['signup'])){
+        showPage('header',['page_title'=>'Pictogram - Signup']);
+        showPage('signup');
+    }
+    showPage('footer');
+    ?>
