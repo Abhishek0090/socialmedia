fee48c2160908917114153ec667d042cda9f1253
Abhishek0090 <palabhishek411@gmail.com>
2022-11-12 18:36:59 +0530

    added config

--- assets/pages/signup.php
+++ assets/pages/signup.php
@@ -1,7 +1,7 @@
  
     <div class="login">
         <div class="col-4 bg-white border rounded p-4 shadow-sm">
-            <form>
+            <form method="post" action="assets/php/actions.php?signup">
                 <div class="d-flex justify-content-center">
 
                     <img class="mb-4" src="assets/images/pictogram.png" alt="" height="45">
@@ -9,26 +9,26 @@
                 <h1 class="h5 mb-3 fw-normal">Create new account</h1>
                 <div class="d-flex">
                     <div class="form-floating mt-1 col-6 ">
-                        <input type="email" class="form-control rounded-0" placeholder="username/email">
+                        <input type="text" class="form-control rounded-0" placeholder="username/email">
                         <label for="floatingInput">first name</label>
                     </div>
                     <div class="form-floating mt-1 col-6">
-                        <input type="email" class="form-control rounded-0" placeholder="username/email">
+                        <input type="text" class="form-control rounded-0" placeholder="username/email">
                         <label for="floatingInput">last name</label>
                     </div>
                 </div>
                 <div class="d-flex gap-3 my-3">
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
-                            value="option1" checked>
-                        <label class="form-check-label" for="exampleRadios1">
+                        <input class="form-check-input" type="radio" name="gender" id="gender"
+                            value="Male" checked>
+                        <label class="form-check-label" for="">
                             Male
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
-                            value="option2">
-                        <label class="form-check-label" for="exampleRadios3">
+                        <input class="form-check-input" type="radio" name="gender" id="gender"
+                            value="Female">
+                        <label class="form-check-label" for="">
                             Female
                         </label>
                     </div>
@@ -41,23 +41,22 @@
                     </div>
                 </div>
                 <div class="form-floating mt-1">
-                    <input type="email" class="form-control rounded-0" placeholder="username/email">
+                    <input type="email" class="form-control rounded-0"  name = "email" id="email" placeholder="username/email">
                     <label for="floatingInput">email</label>
                 </div>
                 <div class="form-floating mt-1">
-                    <input type="email" class="form-control rounded-0" placeholder="username/email">
+                    <input type="text" class="form-control rounded-0"  name="username" id="username"  placeholder="username/email">
                     <label for="floatingInput">username</label>
                 </div>
                 <div class="form-floating mt-1">
-                    <input type="password" class="form-control rounded-0" id="floatingPassword" placeholder="Password">
+                    <input type="password" class="form-control rounded-0" name = "password" id="password" placeholder="Password">
                     <label for="floatingPassword">password</label>
                 </div>
 
                 <div class="mt-3 d-flex justify-content-between align-items-center">
                     <button class="btn btn-primary" type="submit">Sign Up</button>
                     <a href="#" class="text-decoration-none">Already have an account ?</a>
-
-
+ 
                 </div>
 
             </form>
--- assets/php/functions.php
+++ assets/php/functions.php
@@ -1,9 +1,13 @@
 <?php
-
+    require_once 'config.php';
+    $db = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME) or die ('database not connected');
     //function for showing pages
     
     function showPage($page,$data=""){ 
     include("assets/pages/$page.php");
     }
 
+    function validateSignupform($form_data){
+        
+    }
 ?>
